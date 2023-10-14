9fc03c8559d7b3e2e2cc1e2125fc0a09aed9d5f9
Kitalfa <dorian.travers@yahoo.fr>
2023-10-14 09:58:07 +0200

    Fix media quieries

--- assets/js/main.js
+++ assets/js/main.js
@@ -47,8 +47,8 @@ sr.reveal('.about__text', { delay: 400 });
 /*SCROLL SKILLS*/
 sr.reveal('.skills__subtitle', {});
 sr.reveal('.skills__text', {});
-sr.reveal('.skills__data', { interval: 200 });
-sr.reveal('.skills__img', { delay: 600 });
+sr.reveal('.skills__data', { delay: 200, interval: 50 });
+sr.reveal('.skills__img', {});
 
 /*SCROLL WORK*/
 sr.reveal('.work__img', { interval: 200 });
